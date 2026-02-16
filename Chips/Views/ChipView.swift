5dd07e38867b58cf5b14bc81920aa2a99b8bad4a
app-kaisha <app.kaihatsusha@gmail.com>
2026-02-16 20:49:50 +0000

    Dynamic width for chip count

--- Chips/Views/ChipView.swift
+++ Chips/Views/ChipView.swift
@@ -14,14 +14,23 @@ struct ChipView: View {
             VStack {
                 ChipsView {
                     ForEach(mockChips) { chip in
+                        
+                        // calculate chip width for font + 10 padding
+                        let viewWidth = chip.name.size(.preferredFont(forTextStyle: .body)).width + 20
+                        
                         Text(chip.name)
                             .font(.body)
                             .foregroundStyle(.white)
                             .padding(.vertical, 6)
                             .padding(.horizontal, 10)
                             .background(.red.gradient, in: .capsule)
+                            .containerValue(\.viewWidth, viewWidth )
                     }
                 }
+                // TODO: frame width fixed - use geometry reader to pass to ChipsView
+                .frame(width: 300)
+                .padding(15)
+                .background(.primary.opacity(0.06), in: .rect(cornerRadius: 10))
             }
             .padding(15)
             .navigationTitle("Chips")
--- Chips/Views/ChipsView.swift
+++ Chips/Views/ChipsView.swift
@@ -15,12 +15,14 @@ struct ChipsView<Content: View>: View {
     var body: some View {
         // return a collection of subviews based on teh provided view
         Group(subviews: content) { collection in
-            let choppedCollection = collection.chopIntoPieces(2)
+            let choppedCollection = collection.chopByWidth(270)
             
             VStack(alignment: .center, spacing: 10.0) {
                 ForEach(choppedCollection.indices, id:\.self) { index in
                     HStack(spacing: 10.0) {
                         ForEach(choppedCollection[index]) { subview in
+                            //let viewWidth = subview.containerValues.viewWidth
+                            
                             subview
                         }
                     }
@@ -28,9 +30,54 @@ struct ChipsView<Content: View>: View {
             }
         }
     }
+    
+}
+
+extension ContainerValues {
+    // create container value
+    @Entry var viewWidth: CGFloat = 0
+}
+
+extension String {
+    
+    // with font size then get width of chip from width of text
+    func size(_ font: UIFont) -> CGSize {
+        let attributes = [NSAttributedString.Key.font: font]
+        return self.size(withAttributes: attributes)
+    }
 }
 
 extension SubviewsCollection {
+    
+    // containerWidth is total width of chips view
+    // Subview division is divided according to max items possible in a row
+    func chopByWidth(_ containerWidth: CGFloat) -> [[Subview]] {
+        var row: [Subview] = []
+        var rows: [[Subview]] = []
+        // matches spacing in top level view TODO: specify from parameter
+        let spacing: CGFloat = 10
+        var rowWidth: CGFloat = 0
+        
+        for subview in self {
+            let viewWidth = subview.containerValues.viewWidth + spacing
+            
+            rowWidth += viewWidth
+            
+            if rowWidth < containerWidth {
+                row.append(subview)
+            } else {
+                rows.append(row)
+                row = [subview]
+                rowWidth = viewWidth
+            }
+        }
+        
+        if !row.isEmpty {
+            rows.append(row)
+        }
+        
+        return rows
+    }
     
     // split a single array into multiple arrays with specified size
     func chopIntoPieces(_ size: Int) -> [[Subview]] {
