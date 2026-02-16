d4b8bc6d80d55c920624f409c0330c7f591215c5
app-kaisha <app.kaihatsusha@gmail.com>
2026-02-16 19:34:03 +0000

    Initial Commit

---
+++ Chips/ChipsApp.swift
@@ -0,0 +1,18 @@
+//
+//  ChipsApp.swift
+//  Chips
+//
+//  Created by app-kaihatsusha on 16/02/2026.
+//  Copyright © 2026 app-kaihatsusha. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct ChipsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
